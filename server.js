a2513f9c320e382500de825e0851fd37e65e9945
Aman <amanmatreja@gmail.com>
2022-01-10 20:08:05 +0530

    done coding the microservice - might need to test bugs

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const res = require('express/lib/response');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -19,6 +20,11 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/whoami" , (req, res) => {
+  console.log(req.acceptsLanguages(), req.headers)
+
+  res.json({"ipaddress": req.ip ,"language": req.headers['accept-language'], "software": req.headers['user-agent']})
+})
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
